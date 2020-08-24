113d020f31084e2a0c73f7493efda96a122de6e3
Memory Apiyo <52372365+Apiyo21@users.noreply.github.com>
2020-08-24 22:01:04 +0300

    first commit

---
+++ Week 6/docs/index.rst
@@ -0,0 +1,24 @@
+.. Week 6 documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Week 6 documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ Week 6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Bank of Portugal seeks to iddentify customers who potentially would subscribe to their term deposits.As their machine learning researcher,the goal of my employer is to find a model that can predict which future clients who would subscribe to their term deposit. My goal is to carry out data exploration, data cleaning, feature extraction, and developing robust machine learning algorithms that would aid them in the department.',
+    author='Memory Apiyo',
+    license='MIT',
+)
